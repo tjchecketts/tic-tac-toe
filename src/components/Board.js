3447056bf09f2dd18c9b49d4067f722f5ce9a57a
“TJ <tylerchecketts@gmail.com>
2018-03-24 15:24:24 -0600

    setup win and game end

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,26 @@
 import React from 'react'
 import Square from './Square'
 
+const winLogic = (squares) => {
+  const lines = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
 class Board extends React.Component {
   state = { 
     squares: [Array(9).fill(null)],
@@ -9,6 +29,9 @@ class Board extends React.Component {
 
   handleClick = (i) => {
     const squares = this.state.squares.slice()
+    if (winLogic(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? "X" : "O"
     this.setState({ 
       squares: squares,
@@ -16,7 +39,7 @@ class Board extends React.Component {
     })
   }
 
-  renderSquare(i) {
+  renderSquare = (i) => {
     return (
       <Square 
         value={this.state.squares[i]}
@@ -26,25 +49,31 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${ this.state.xIsNext ? "X" : "O" }`
+    const winner = winLogic(this.state.squares)
+    let status
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next player: ${ this.state.xIsNext ? "X" : "O" }`
+    }
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
+          {this.renderSquare(3)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
+          {this.renderSquare(6)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
+          {this.renderSquare(9)}
         </div>
       </div>
     )
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 const Square = (props) => {
   return (
-    // works: onClick={ () => { props.onClick() } }
-    // doesn't work onClick={props.onClick()} 
     <button className="square" onClick={ props.onClick }>
       { props.value }
     </button>
