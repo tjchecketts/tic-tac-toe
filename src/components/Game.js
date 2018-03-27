daaff0361efd7b8c8f238d9dce11c6de61ab939b
“TJ <tylerchecketts@gmail.com>
2018-03-27 16:18:42 -0600

    add rewind ability

--- src/components/Game.js
+++ src/components/Game.js
@@ -26,11 +26,13 @@ class Game extends React.Component {
     history: [{
       squares: [Array(9).fill(null)],
     }],
+    stepNumber: 0,
     xIsNext: true,
   }
 
   handleClick = (i) => {
     const history = this.state.history
+      .slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
     if (winLogic(squares) || squares[i]) {
@@ -41,14 +43,35 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    })
+  }
+
+  jumpTo = (step) => {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     })
   }
 
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = winLogic(current.squares)
+
+    const moves = history.map( (step, move) => {
+      const desc = move ?
+        `Go to move #${move}` :
+        "Go to game start"
+      return (
+        <li key={move}>
+          <button onClick={ () => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+      )
+    })
 
     let status
     if (winner) {
@@ -67,7 +90,7 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <ol>{/* TODO */}</ol>
+          <ul>{ moves }</ul>
         </div>
       </div>
     )
